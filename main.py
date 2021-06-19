69acf00fbb8a7e3c3bd0e669973f8ce06a528073
michelle4232 <62208026+michelle4232@users.noreply.github.com>
2021-06-19 15:09:38 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from linebot.exceptions import (
 from linebot.models import *
 
 import re
+import os
+from flask import Flask, request, abort
+import requests
+import json
+
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -20,6 +25,39 @@ handler = WebhookHandler('1e9a05fa42febda9fc4f8e629dfe2e75')
 #line_bot_api.push_message('Ue0e081b868223a8e25b8b3cd7898611d', TextSendMessage(text='你可以開始了'))
 
 print('aaa')
+
+
+def GetWeather(station):
+    end_point = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-6B01579C-E0F7-4DE2-A51A-AD5FDFFF709F"
+
+    data = requests.get(end_point).json()
+    data = data["records"]["location"]
+
+    target_station = "not found"
+    for item in data:
+        if item["locationName"] == str(station):
+            target_station = item
+    return target_station
+
+
+def MakeWeather(station):
+    WeatherData = GetWeather(station)
+    if WeatherData == "not found":
+        return False
+
+    msg = "天氣報告 - " + station
+    # for i in range(5):
+    # WeatherData = WeatherData["weatherElement"][i]["time"][0]["parameter"]["parameterName"]
+    msg += "\n" + WeatherData["weatherElement"][0]["time"][0]["parameter"]["parameterName"] + "\n"
+    msg += "降雨率 = " + WeatherData["weatherElement"][1]["time"][0]["parameter"]["parameterName"] + "%\n"
+    msg += "最低溫 = " + WeatherData["weatherElement"][2]["time"][0]["parameter"]["parameterName"] + "℃\n"
+    msg += "體感 = " + WeatherData["weatherElement"][3]["time"][0]["parameter"]["parameterName"] + "\n"
+    msg += "最高溫 = " + WeatherData["weatherElement"][4]["time"][0]["parameter"]["parameterName"] + "℃\n"
+    #msg += MakeAQI(station)
+    print(msg)
+    return msg
+
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -43,14 +81,28 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mtext = text=event.message.text
+    cmd = event.message.text.split(" ")
+    '''
     if mtext == '告訴我秘密':
         image_message = ImageSendMessage(
             original_content_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2F9c8c1e647eb14e01898043c0c60bf03a&methods=resize%2C1000%2C5000',
             preview_image_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2Ffd2258c5ea6c43f591e8d9930d152b94&methods=resize%2C1000%2C5000'
         )
         line_bot_api.reply_message(event.reply_token, image_message)
-    elif mtext == '你好':
+    '''
+
+    if mtext == '你好':
         line_bot_api.reply_message(event.reply_token, '嗨嗨')
+    elif cmd[0] == "天氣":
+        station = cmd[1]
+        WeatherMsg = MakeWeather(station)
+
+        if not WeatherMsg:
+            WeatherMsg = "沒這個氣象站!"
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=WeatherMsg))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(mtext))
 
