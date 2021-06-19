f038ccc753b6a7c140be13cdd5cb60cf5cdee7ce
michelle4232 <62208026+michelle4232@users.noreply.github.com>
2021-06-19 15:03:04 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -42,15 +42,17 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = text=event.message.text
-    if re.match('告訴我秘密',message):
+    mtext = text=event.message.text
+    if mtext == '告訴我秘密':
         image_message = ImageSendMessage(
             original_content_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2F9c8c1e647eb14e01898043c0c60bf03a&methods=resize%2C1000%2C5000',
             preview_image_url='https://images.builderservices.io/s/cdn/v1.0/i/m?url=https%3A%2F%2Fstorage.googleapis.com%2Fproduction-bluehost-v1-0-9%2F659%2F790659%2FAtmP8Pmy%2Ffd2258c5ea6c43f591e8d9930d152b94&methods=resize%2C1000%2C5000'
         )
         line_bot_api.reply_message(event.reply_token, image_message)
+    elif mtext == '你好':
+        line_bot_api.reply_message(event.reply_token, '嗨嗨')
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(mtext))
 
 #主程式
 import os
