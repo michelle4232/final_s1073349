59642be40af6b3926d226d262f44c6e80b9836e8
michelle4232 <62208026+michelle4232@users.noreply.github.com>
2021-06-19 11:46:17 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#載入LineBot所需要的套件
 from flask import Flask, request, abort
 
 from linebot import (
@@ -7,7 +8,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
+import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
@@ -16,7 +17,6 @@ line_bot_api = LineBotApi('ra82QMewyxn2gn9uZkwidIc1QNxhKiRQvSFeDxath6YR1OzzyWANu
 handler = WebhookHandler('a0b4d2d97a0b66c6e6e0b90c3394c906')
 
 line_bot_api.push_message('Ue0e081b868223a8e25b8b3cd7898611d', TextSendMessage(text='你可以開始了'))
-
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -36,18 +36,22 @@ def callback():
 
     return 'OK'
 
-
-# 訊息傳遞區塊
+#訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
-
-
-# 主程式
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        # 貼圖查詢：https://developers.line.biz/en/docs/messaging-api/sticker-list/#specify-sticker-in-message-object
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+        line_bot_api.reply_message(event.reply_token, sticker_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
 import os
-
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
