1632cdba8f26e6a7c5def6a91368493864be234d
michelle4232 <62208026+michelle4232@users.noreply.github.com>
2021-06-19 20:52:32 +0800

    Add files via upload

--- googleTrans.py
+++ googleTrans.py
@@ -3,8 +3,10 @@ from google_trans_new import google_translator
 def text_to_translate(text):
     tran = ''
     for txt in text:
+        if txt == '@翻譯':
+            continue
         tran += txt + ''
-
+    print(tran)
     translator = google_translator()
     translate_text = translator.translate(tran, lang_tgt='zh-tw')
     return translate_text
